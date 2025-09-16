ad4c729fe0e881d73746ba4e403669aba99a38e6
bivisualizer <info@bivisualizer.com>
2025-09-16 19:06:07 -0300

    cores planejado e pausado

--- meu-sistema/frontend/assets/js/main.js
+++ meu-sistema/frontend/assets/js/main.js
@@ -1109,7 +1109,8 @@
       case 'Em Risco': return 'bg-red-100 text-red-800';
       case 'Concluído': return 'bg-blue-100 text-blue-800';
       case 'Sustentação': return 'bg-yellow-100 text-yellow-800';
-      case 'Planejado': return 'bg-gray-100 text-gray-800';
+      case 'Planejado': return 'bg-purple-100 text-purple-800';
+      case 'Pausado': return 'bg-gray-300 text-gray-800';
       default: return 'bg-gray-100 text-gray-800';
     }
   }
